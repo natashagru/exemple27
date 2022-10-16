31c68000ce74317ef832c15fbb08175b43a28c40
natalia <natka545@mail.ru>
2022-10-16 19:37:17 +0300

    решили задачу 27

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите функцию и запустите ее, которая принимает на вход число и выдает сумму цифр в числе
+
+int Sum(int A)
+{
+    int sum = 0;
+    for (int i = 0; A > 0; i++)
+    {
+    sum = sum + A%10;
+    A = A/10;
+    }
+return sum;
+}
+Console.Clear();
+Console.Write("введите A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum(A));
